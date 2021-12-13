c247c6025a5b359082dc1252b61c461a709b370d
Brandin Canfield <bcanfield@miletwo.us>
2021-12-13 10:24:08 -0500

    Add standalone express server

--- nextjs/server.js
+++ nextjs/server.js
@@ -1,128 +1,36 @@
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const compression = require("compression");
-const { graphqlHTTP } = require("express-graphql");
-const cors = require("cors");
-const gql = require("graphql-tag");
-// Subscriptions
-const ws = require("ws");
-const { useServer } = require("graphql-ws/lib/use/ws");
-const { execute, subscribe } = require("graphql");
+const http = require("http");
 const { getBuiltMesh } = require("./.mesh");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-const app = express();
-// const schema = require("./src/schema");
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!",
-  },
-};
-
-// const { rolesDataLoader } = require('./src/loaders');
-async function handler() {
-  app.use(cors());
-  app.use(express.static("public"));
-  // app.use(require("./src/middleware/auth-middleware"));
-  app.use(compression());
-
-  // const schema = makeExecutableSchema({ typeDefs, resolvers });
-
+async function main() {
   const { schema } = await getBuiltMesh();
-  app.get("/", (req, res) => res.send("GraphQL Server is running"));
 
-  const PORT = process.env.PORT;
+  const app = express();
+  const server = new ApolloServer({
+    schema,
+  });
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: true,
+    },
+    async onHealthCheck() {
+      return;
+    },
+  });
+  const httpServer = http.createServer(app);
+  server.installSubscriptionHandlers(httpServer);
 
-  // https://github.com/graphql/express-graphql
-  // If no context is created here, the request object is passed instead
-  app.use(
-    "/graphql",
-    graphqlHTTP((req) => ({
-      schema,
-      graphiql: {
-        // defaultQuery: require("./default-query"),
-        headerEditorEnabled: true,
-      },
-      context: {
-        isAuth: req.isAuth,
-        user: req.user,
-        error: req.error,
-        // loaders: {
-        //     rolesLoader: rolesDataLoader
-        // }
-      },
-      customFormatErrorFn: (err) => {
-        if (!err.originalError) {
-          return err;
-        }
-        /* 
-            You can add the following to any resolver
-            const error = new Error('My message')
-            error.data = [...]
-            error.code = 001
-        */
-        const message = err.message || "An error occured.";
-        const code = err.originalError.code;
-        const data = err.originalError.data;
-        return {
-          // ...err,
-          message,
-          code,
-          data,
-        };
-      },
-    }))
-  );
-
-  const server = app.listen(PORT, () => {
-    const sAddress = server.address();
+  const port = process.env.APP_PORT || 4000;
+  httpServer.listen({ port, host: "0.0.0.0" }, () => {
     console.log(
-      `[${process.env.NODE_ENV}] GraphQL Server running on http://localhost:${sAddress.port}/graphql`
+      `🚀 Server ready at http://0.0.0.0:${port}${server.graphqlPath}`
     );
-
-    // GRAPHQL-WS
-    // if (process.env.USE_WS) {
-    // create and use the websocket server
-    const path = "/subscriptions";
-    const wsServer = new ws.Server({
-      server,
-      path,
-    });
-
-    useServer(
-      {
-        schema,
-        execute,
-        subscribe,
-        onConnect: (ctx) => {
-          console.log("Connect");
-        },
-        onSubscribe: (ctx, msg) => {
-          console.log("Subscribe");
-        },
-        onNext: (ctx, msg, args, result) => {
-          console.debug("Next");
-        },
-        onError: (ctx, msg, errors) => {
-          console.error("Error");
-        },
-        onComplete: (ctx, msg) => {
-          console.log("Complete");
-        },
-      },
-      wsServer
-    );
-    const wsAddress = wsServer.address();
     console.log(
-      `[${process.env.NODE_ENV}] WebSockets listening on ws://localhost:${wsAddress.port}${path}`
+      `🚀 Subscriptions ready at ws://0.0.0.0:${port}${server.subscriptionsPath}`
     );
-    // }
   });
 }
 
-handler();
+main().catch(console.error);
